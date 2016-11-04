8e5c4d55f2ae7e4c5dd85ebf8c01456205d2f114
CodeToSurvive1 <lixiaojiao_hit@163.com>
2016-11-04 15:01:15 +0800

    Update maidanba_android_webview_device.js

--- macaca-test/maidanba_android_webview_device.js
+++ macaca-test/maidanba_android_webview_device.js
@@ -27,7 +27,9 @@ var androidOpts = {
   platformName: 'Android',
   package: 'com.bankcomm.maidanba',
   reuse: 2,
-  udid: '2353c4bb',
+//   udid: '2353c4bb',
+  udid: 'PBV0216706008526',
+  
   activity: '.activity.SplashActivity',
   // activity: 'com.bankcomm.maidanba/.activity.SplashActivity',
   // app: path.join(__dirname, '..', 'app', `${platform}-app-bootstrap.zip`)
