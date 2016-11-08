054d49f416323fe43a839be371b9519d42de2826
Administrator <lixiaojiao_hit@163.com>
2016-11-08 13:01:42 +0800

    11

--- macaca-test/testTianMao.js
+++ macaca-test/testTianMao.js
@@ -14,9 +14,9 @@ var iOSOpts = {
   udid: '9daef85429600e88ea5442ba3d5fee548c02aeac',
   // bundleId: 'xudafeng.ios-app-bootstrap.1'
   bundleId: 'com.bankcomm.maidanba'
-    // bundleId: 'com.csii.csbank'
-    // bundleId: 'com.tencent.mm'
-    // bundleId: 'com.tencent.xin'
+  // bundleId: 'com.csii.csbank'
+  // bundleId: 'com.tencent.mm'
+  // bundleId: 'com.tencent.xin'
 
 
 
@@ -25,12 +25,12 @@ var iOSOpts = {
 
 var androidOpts = {
   platformName: 'Android',
-  package: 'com.bankcomm.maidanba',
+  package: 'com.tmall.wireless',
   reuse: 2,
-  udid: 'DU2MYN146D002912',
-  activity: '.activity.SplashActivity',
+  udid: process.env.udid || '23051d7b',
+  activity: '.maintab.module.TMMainTabActivity',
   // activity: 'com.bankcomm.maidanba/.activity.SplashActivity',
-  // app: path.join(__dirname, '..', 'app', `${platform}-app-bootstrap.zip`)
+  // app: path.join(__dirname, '..', 'app', `tianmao.apk`)
 };
 
 var wd = require('webdriver-client')(_.merge({}, platform === 'ios' ? iOSOpts :
@@ -90,31 +90,102 @@ describe('macaca mobile sample', function() {
   // });
 
 
-  it('#1 点击生活按钮', function() {
+  it('#1 关注', function() {
     return driver
       .sleep(2000)
-      .waitForElementByName('生活')
+      .waitForElementByName('关注')
       .click()
       .sleep(3000);
   });
 
-
-  it('#2 点击商旅出行', function() {
+  it('#2 购物车', function() {
     return driver
-      .elementByName('商旅出行')
+      .sleep(2000)
+      .waitForElementByName('购物车')
       .click()
       .sleep(3000);
   });
 
-
-  it('#3 点击换旅程', function() {
+  it('#3 我', function() {
     return driver
-      .webview()
-      .elementByXPath(
-        '//*[@id="layout"]/div[2]/div[3]/ul/li[1]/a')
+      .sleep(2000)
+      .waitForElementByName('我')
       .click()
       .sleep(3000);
   });
+
+  it('#4 天猫', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('天猫')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#5 关注', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('关注')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#6 我', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('我')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#7 购物车', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('购物车')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#8 天猫', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('天猫')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#9 我', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('我')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#10 关注', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('关注')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#11 天猫', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('天猫')
+      .click()
+      .sleep(3000);
+  });
+
+  it('#12 我', function() {
+    return driver
+      .sleep(2000)
+      .waitForElementByName('我')
+      .click()
+      .sleep(3000);
+  });
+
 
 
 
